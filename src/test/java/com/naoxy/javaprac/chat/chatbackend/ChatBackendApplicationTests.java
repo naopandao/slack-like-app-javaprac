2a73e85498b3201dd92284fdace9624d67907e93
naopandao <nao_yoshida@yunixy.co.jp>
2023-03-04 15:06:15 +0900

    first commit

---
+++ src/test/java/com/naoxy/javaprac/chat/chatbackend/ChatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naoxy.javaprac.chat.chatbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
